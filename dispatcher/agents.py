9416b8a523ff10f2e8521d159caae7b9fb782a6c
BeneficialEffect <awais@li.nux.org.uk>
2019-01-05 17:32:44 +0000

    The new dispatcher class - added scope

--- dispatcher/agents.py
+++ dispatcher/agents.py
@@ -5,8 +5,10 @@ class Agent:
     def __init__(self, name):
         self.name = name
         self.available = True
+
     # AVAILABILITY
     def begincall(self):
         self.available = False
+
     def endcall(self):
         self.available = True
